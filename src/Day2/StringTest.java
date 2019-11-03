fe41507abb916b6033ec0c07215050bceed64538
Tang <a2576445561@qq.com>
2019-11-03 21:02:09 +0800

    Day2 submit

---
+++ src/Day2/StringTest.java
@@ -0,0 +1,17 @@
+package Day2;
+
+import java.io.Console;
+
+/**
+ * @Author TangXi
+ * @Date 19-11-3 下午8:39
+ */
+public class StringTest {
+	public static void main(String[] args) {
+		String name = "张三";
+		System.out.println(name.hashCode());
+		name = "李四";
+		System.out.println(name);
+		System.out.println(name.hashCode());
+	}
+}
